a466f24f66593986dd91114a21a861625dd216ae
Falestio <falestioltmpt@gmail.com>
2023-01-03 01:29:42 +0700

    list of content

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>The Exercise Are</p>
+        <ul>
+            <li>
+                <NuxtLink>Pilihan Ganda</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink>Persons Problems</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink>IDE</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink>Projects with CLI App</NuxtLink>
+            </li>
+        </ul>
+    </div>
+</template>
